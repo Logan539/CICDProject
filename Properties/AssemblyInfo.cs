f22f0becd668606c50436f1691c2a8b79650da0a
Girish Gaikwad <Girish Gaikwad@LAPTOP-2S3N9ROM>
2023-11-28 21:59:32 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingCICD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("TestingCICD")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef5a9e1a-e966-48f1-a467-5a2e8a8d417b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace TestingCICD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private string appUrl;
+        public TestContext testContext;
+
+        public UnitTest1()
+        {
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void TheBingSearchTest()
+        {
+            driver.Navigate().GoToUrl(appUrl);
+            driver.Manage().Window.Maximize();
+            IWebElement ele = driver.FindElement(By.Id("sb_form_q"));
+            ele.SendKeys("Azure Pipelines");
+            ele.SendKeys(Keys.Enter);
+            driver.FindElement(By.XPath("//ol[@id='b_results']/li/div[2]/h2/a")).Click();
+            Assert.IsTrue(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
+        }
+
+        public TestContext TestContext
+        {
+            get { return testContext; }
+            set { testContext = value; }
+        }
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            appUrl = "https://www.bing.com/";
+            string browser = "Chrome";
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "Edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            driver.Quit();
+        }
+    }
+}
